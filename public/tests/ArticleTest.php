8844de1f04e2acf40053a2150044ac77d74599c2
alex <o.lototskyi@gmail.com>
2023-02-12 21:40:56 +0200

    using dataProvider

--- public/tests/ArticleTest.php
+++ public/tests/ArticleTest.php
@@ -22,28 +22,51 @@ class ArticleTest extends TestCase
         $this->assertSame("", $this->article->getSlug());
     }
 
-    public function testSlugHasSpacesReplacedByUnderscores()
+//    public function testSlugHasSpacesReplacedByUnderscores()
+//    {
+//        $this->article->setTitle('An example article');
+//        $this->assertEquals('An_example_article', $this->article->getSlug());
+//    }
+//
+//    public function testSlugHasWhitespaceReplacedBySingleUnderscore()
+//    {
+//        $this->article->setTitle("An    example   \n   article");
+//        $this->assertEquals("An_example_article", $this->article->getSlug());
+//    }
+//
+//    public function testSlugDoesNotStartOrEndWithAnUnderscore()
+//    {
+//        $this->article->setTitle(" An example article ");
+//        $this->assertEquals("An_example_article", $this->article->getSlug());
+//    }
+//
+//    public function testSlugDoesNotHaveAnyNonWordCharacters()
+//    {
+//        $this->article->setTitle("Read! This! Now!");
+//        $this->assertEquals("Read_This_Now", $this->article->getSlug());
+//    }
+
+    public static function titleProvider(): array
     {
-        $this->article->setTitle('An example article');
-        $this->assertEquals('An_example_article', $this->article->getSlug());
+        return [
+            'Slug Has Spaces Replaced By Underscores'
+                            => ["An example article", "An_example_article"],
+            'Slug Has Whitespace Replaced By Single Underscore'
+                            => ["An    example   \n   article", "An_example_article"],
+            'Slug Does Not Start Or End With An Underscore'
+                            => [" An example article ", "An_example_article"],
+            'Slug Does Not Have Any Non Word Characters'
+                            => ["Read! This! Now!", "Read_This_Now"]
+        ];
     }
 
-    public function testSlugHasWhitespaceReplacedBySingleUnderscore()
+    /**
+     * @dataProvider titleProvider
+     */
+    public function testSlug($title, $slug)
     {
-        $this->article->setTitle("An    example   \n   article");
-        $this->assertEquals("An_example_article", $this->article->getSlug());
-    }
-
-    public function testSlugDoesNotStartOrEndWithAnUnderscore()
-    {
-        $this->article->setTitle(" An example article ");
-        $this->assertEquals("An_example_article", $this->article->getSlug());
-    }
-
-    public function testSlugDoesNotHaveAnyNonWordCharacters()
-    {
-        $this->article->setTitle("Read! This! Now!");
-        $this->assertEquals("Read_This_Now", $this->article->getSlug());
+        $this->article->setTitle($title);
+        $this->assertEquals($slug, $this->article->getSlug());
     }
 
 }
