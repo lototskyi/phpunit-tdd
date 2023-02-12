615c65bc11bc73f88f1af0b4b45610da655e542e
alex <o.lototskyi@gmail.com>
2023-02-12 19:49:07 +0200

    first test for class Article

---
+++ public/tests/ArticleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class ArticleTest extends TestCase
+{
+    public function testTitleIsEmptyByDefault()
+    {
+        $article = new App\Article;
+
+        $this->assertEmpty($article->title);
+    }
+}
